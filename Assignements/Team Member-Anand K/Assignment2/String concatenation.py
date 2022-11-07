84bafc37dbf250a4d32c377695e0990d6ebb27d9
Anand-1205 <110402472+Anand-1205@users.noreply.github.com>
2022-11-07 10:30:00 +0530

    Create String concatenation.py

---
+++ Assignements/Team Member-Anand K/Assignment2/String concatenation.py
@@ -0,0 +1,19 @@
+str1="hello"
+str2="world"
+
+str3=str1+str2
+print(str3)
+
+
+     
+   #using join method
+   
+str1 = "Hello"  
+str2 = "world" 
+   
+print("".join([str1, str2]))  
+  
+# the string with a separator Space(" ")  
+str3 = " ".join([str1, str2])  
+  
+print(str3) 
